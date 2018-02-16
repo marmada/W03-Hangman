300cdb732197801c9ed351429bd6d44661e07372
Miriam Armada <miriam_armada@yahoo.com>
2018-02-15 22:39:53 -0700

    fixed JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,63 @@
-// First Set up Array for Letters A-Z
+// JS File
+
 var arrAbc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Then define counters for Win, Loose, # guesses, #opportunities left
+ //Then define counters for Win, Loose, # guesses, #opportunities left
 
 var randomLetter = " ";
 var userguess = " ";
-var incorrect = [];
+var ansArr = [];
 var winCounter = 0;
 var lostCounter = 0;
 var guessCounter = 0;
 var oppCounter = 9;
 
 
-// Set up Initial Game Scores - pending
+ //Set up Initial Game Scores - pending
 
-// randomizer
 
 function CompGame() {
 
-    //Computer with Random funtion will pick a letter randomly for a number between 0 & 25, assign value to
+   // Computer with Random funtion will pick a letter randomly for a number between 0 & 25, assign value to
     var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
     console.log(randomLetter); //remove this after game is running
+
+     // Set Counters to start point
+
+    document.getElementById("win").innerHTML = winCounter; 
+    document.getElementById("lost").innerHTML = lostCounter;
+    document.getElementById("guessP").innerHTML = guessCounter;
+    document.getElementById("chancesLeft").innerHTML = oppCounter;
 }
 
+   
+  
 function userGame() {
 
-    // c) Promt Player for input, 
+   //Promt Player for input, 
     var userguess = prompt("Enter a letter between A & Z");
-
     console.log(userguess); 
-    
-    // remove this after game is running
-    document.getElementById("win").innerHTML = winCounter; 
-    
-    // win is the id from the HTML Element
- 
-    //document.getElementById("yourPicks").innerHTML = incorrect;
+    ansArr.push(userguess);
+    console.log(ansArr);
+    document.getElementById("yourPicks").innerHTML = ansArr;
+}
 
+
+/*
+   
+    
+
+   // Winning Algorithm
     if (randomLetter == userguess) {
 
-        //Winning Algorithm
+   
 
         winCounter++;
         console.log(winCounter);
     
         alert("You have won " + winCounter + " games");
 
-        //  getElementById("win").innerHtml = winCounter;
+          getElementById("win").innerHtml = winCounter;
 
         var playAgain = confirm("Do you want to play again, click on the Magic Ball?");
         if (playAgain = true) {
@@ -57,11 +68,11 @@ function userGame() {
         }
         else {
             alert("'Game Over! You Won ' + winCounter + ' times, You Lost ' + lostCounter + ' times'");
-            //getElementbyid(IMAGE), change for a GAME OVER 
+            getElementbyid(IMAGE), change for a GAME OVER 
 
         }
 
-        //Try Again
+        Try Again
     }
     else {
         guessCounter++;
@@ -79,7 +90,7 @@ function userGame() {
         }
     }
 }
-
+/*
 function ResetGame() {
 
     alert("Game Over! You Have Won " + winCounter + " times, You Have Lost " + lostCounter + " times");
@@ -102,12 +113,14 @@ function ResetGame() {
 CompGame();
 userGame();
 
-     //
+     
 
 
-    //if input = to Randomw Variable: winCounter++, Alert Player "You won", prompt play again? , go back to random funtion
-    //if not guess counter ++, oppCounter-1, cycle runs until winCounter is true or guessCounter = 9
-//    if Guess counter is = to 9 then lostCounter + 1, Alert Player "you lost" play again? OK prompt randomize, pick new value, cancel "GameOVer" , reset value
+    if input = to Randomw Variable: winCounter++, Alert Player "You won", prompt play again? , go back to random funtion
+    if not guess counter ++, oppCounter-1, cycle runs until winCounter is true or guessCounter = 9
+    if Guess counter is = to 9 then lostCounter + 1, Alert Player "you lost" play again? OK prompt randomize, pick new value, cancel "GameOVer" , reset value
 
-
+*/
+document.getElementById("start").addEventListener("click", CompGame);
+document.getElementById("guessB").addEventListener("click", userGame);
 
