c5c2100ffeab725edd2d2e6de2197a5518454d4a
Miriam Armada <miriam_armada@yahoo.com>
2018-02-17 01:42:27 -0700

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,83 +2,92 @@
 
 var arrAbc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
- //Then define counters for Win, Loose, # guesses, #opportunities left
+//Then define counters for Win, Loose, # guesses, #opportunities left
 
-var randomLetter = " ";
-var userguess = " ";
+
 var ansArr = [];
 var winCounter = 0;
 var lostCounter = 0;
 var guessCounter = 0;
 var oppCounter = 9;
+var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
+console.log(randomLetter); //remove this after game is running
 
 
- //Set up Initial Game Scores - pending
+
+document.onkeyup = function(event) {
 
 
-function CompGame() {
-
-   // Computer with Random funtion will pick a letter randomly for a number between 0 & 25, assign value to
-    var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
-    console.log(randomLetter); //remove this after game is running
-
-     // Set Counters to start point
-
-    document.getElementById("win").innerHTML = winCounter; 
+    var userGuess = event.key;
+    console.log(randomLetter);
+    document.getElementById("userEntry").value = " ";
+    var userGuessL = userGuess.toLowerCase();
+    document.getElementById("win").innerHTML = winCounter;
     document.getElementById("lost").innerHTML = lostCounter;
-    document.getElementById("guessP").innerHTML = guessCounter;
-    document.getElementById("chancesLeft").innerHTML = oppCounter;
-}
-
-   
-  
-function userGame() {
-
-   //Promt Player for input, 
-    var userguess = prompt("Enter a letter between A & Z");
-    console.log(userguess); 
-    ansArr.push(userguess);
+    console.log(userGuessL);
+    ansArr.push(userGuessL);
     console.log(ansArr);
     document.getElementById("yourPicks").innerHTML = ansArr;
+    guessCounter++;
+    console.log(guessCounter);
+    document.getElementById("guessP").textContent = guessCounter;
+    oppCounter--;
+    console.log(oppCounter);
+    document.getElementById("chancesLeft").textContent = oppCounter;
 
-    if (randomLetter === userguess) {
+    if (randomLetter == userGuessL) {
+
         winCounter++;
         console.log(winCounter);
         document.getElementById("win").innerHtml = winCounter;
-        alert("GameOver!! Press Enter to Reset the Game")
-
-    
-        
-          
+        var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
+        console.log(randomLetter); //remove this after game is running
+        console.log(guessCounter);
+        document.getElementById("guessP").innerHtml = guessCounter;
+        guessCounter=0;
+        oppCounter=9;
+        document.getElementById("gameOutput").textContent = "You won! Im ready, enter your new guess to keep playing";
+        resetGame();
     }
     else {
-        guessCounter++;
-        console.log(guessCounter);
-        document.getElementById("guessP").innerHtml = winCounter;
-        oppCounter--;
         console.log(oppCounter);
-        document.getElementById("chancesLeft").innerHtml = winCounter;
+        document.getElementById("chancesLeft").textContent = oppCounter;
+        console.log(guessCounter);
+        document.getElementById("guessP").textContent = guessCounter;
+    
+        if (guessCounter < 9) {
 
-        if (guessCounter === 9) {
+            document.getElementById("gameOutput").textContent = "keep trying!";
+            
+        }
+
+
+        else {
 
             lostCounter++;
-            alert("Click on Reset to Play Again")
-        }
-
-        else{
-            userGame();
+            var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
+            console.log(randomLetter); //remove this after game is running
+            document.getElementById("gameOutput").textContent = "You Loose! enter your new guess to keep playing";
+            var randomLetter = arrAbc[Math.floor(Math.random() * arrAbc.length + 1)];
+            console.log(randomLetter); //remove this after game is running
+            resetGame()
         }
     }
+
+
 }
 
-function ResetGame() {
-    
+
+
+function resetGame() {
+
     guessCounter = 0;
     oppCounter = 9;
     ansArr.splice(0, 9);
-    alert("To play again, click on the Start sign")
-    }
 
-document.getElementById("start").addEventListener("click", CompGame);
-document.getElementById("guessB").addEventListener("click", userGame);
-document.getElementById("reset").addEventListener("click", ResetGame);
+}
+
+
+document.getElementById("reset").addEventListener("click", resetGame);
+
+
